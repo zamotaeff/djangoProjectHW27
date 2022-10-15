935d04a5c7f76b1b75ccf9ca79ef57474e53be94
levi.german <levi.german@yandex.ru>
2022-10-15 17:04:17 +0300

    Fox imports

--- project/ads/management/commands/csv_to_json.py
+++ project/ads/management/commands/csv_to_json.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 
-from project.ads.helpers import convert_ads_csv_to_json, convert_categories_csv_to_json
+from ads.helpers import convert_ads_csv_to_json, convert_categories_csv_to_json
 
 
 class Command(BaseCommand):
